0ccb1deedd88e6cb6f8826b77b7c1ce27e20e7b8
Patel Rudra <patelrudra0343@gmail.com>
2025-02-11 21:35:40 +0530

    change

--- src/components/FilterCard.jsx
+++ src/components/FilterCard.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { RadioGroup, RadioGroupItem } from './ui/radio-group'
+
+const fitlerData = [
+  {
+    fitlerType:"Location",
+    Array:["Delhi NCR", "Bangalore", "Hyderabad", "Pune", "Mumbai"]
+  },
+  {
+    fitlerType:"Industry",
+    Array:["Frontend Developer", "Backend Developer", "Full Stack Developer", "DevOps", "Data Science"]
+  },
+  {
+    fitlerType:"Salary",
+    Array:["0-40k", "42k to 1lakh", "1lakh to 5lakh"]
+  },
+]
 
 const FilterCard = () => {
   return (
     <div>
-        
+        <h1>Filter Jobs</h1>
+        <hr className='mt-3'/>
+        <RadioGroup>
+          {
+            fitlerData.map((data, index) => (
+              <div>
+                <h1>{data.fitlerType}</h1>
+                {
+                  data.Array.map((item, index) => (
+                      <div key={index}>
+                        <RadioGroupItem value={item}/>
+                        <label>{item}</label>
+                      </div>
+                  ))
+                }
+              </div>
+            ))
+          }
+        </RadioGroup>
     </div>
   )
 }
--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Button } from './ui/button'
 import { Bookmark } from 'lucide-react'
 import { Avatar, AvatarImage } from './ui/avatar'
+import { Badge } from './ui/badge'
 
 const Job = () => {
   return (
-    <div>
+    <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100" >
       <div className='flex justify-between items-center'>
         <p>2 days ago</p>
         <Button variant="outline" className="rounded-full" size="icon"><Bookmark /></Button>
@@ -17,9 +18,22 @@ const Job = () => {
           </Avatar>
         </Button>
         <div>
-          <h1>Company Name</h1>
-          <p>India</p>
+          <h1 className='font-medium text-lg'>Company Name</h1>
+          <p className='text-sm text-gray-500'>India</p>
         </div>
+      </div>
+      <div>
+        <h1 className='font-bold text-lg my-2'>Title</h1>
+        <p className='text-sn text-gray-500'>Lorem ipsum dolor sit amet consejfcss, fqejflefe, fbwiyfew. he nfenfenfewnf.</p>
+      </div>
+      <div className='flex items-center gap-2 mt-4'> 
+      <Badge className={'text-blue-700 font-bold'} variant="ghost">12 Positions</Badge>
+      <Badge className={'text-[#F83002] font-bold'} variant="ghost">Part Time</Badge>
+      <Badge className={'text-[#7209B7] font-bold'} variant="ghost">24LPA</Badge>
+      </div>
+      <div className='flex items-center gap-4 mt-4'>
+        <Button variant="outline">Details</Button>
+        <Button className="bg-[#000000]">Save For Later</Button>
       </div>
     </div>
   )
