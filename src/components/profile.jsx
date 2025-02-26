f383d862f690a9bfe730fd77cd879a720cb0583f
Patel Rudra <patelrudra0343@gmail.com>
2025-02-26 09:12:47 +0530

    Change

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -2,23 +2,59 @@ import React from 'react'
 import Navbar from './shared/Navbar'
 import { Avatar, AvatarImage } from './ui/avatar'
 import { Button } from './ui/button'
-import { Pen } from 'lucide-react'
+import { Contact, Mail, Pen } from 'lucide-react'
+import { Badge } from './ui/badge'
+import { Label } from './ui/label'
+import AppliedJobTable from './AppliedJobTable'
 
+const skills = ["Html", "css", "Javascript", "Rectjs"]
 const profile = () => {
+
+    const isResume = true;
     return (
         <div>
             <Navbar />
-            <div className='max-w-7xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8'>
-                <div className='flex gap-5 items-center'>
-                    <Avatar className='h-24 w-24'>
-                        <AvatarImage src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg" alt="profile" />
-                    </Avatar>
-                    <div>
-                        <h1 className='font-medium text-xl'>Full Name</h1>
-                        <p>Add Your Detail Here.</p>
+            <div className='max-w-4xl mx-auto bg-white border border-gray-200 rounded-2xl my-5 p-8'>
+                <div className='flex justify-between'>
+                    <div className='flex gap-5 items-center'>
+                        <Avatar className='h-24 w-24'>
+                            <AvatarImage src="https://www.shutterstock.com/image-vector/circle-line-simple-design-logo-600nw-2174926871.jpg" alt="profile" />
+                        </Avatar>
+                        <div>
+                            <h1 className='font-medium text-xl'>Full Name</h1>
+                            <p>Add Your Detail Here.</p>
+                        </div>
                     </div>
-                    <Button className="text-right" variant="outline"><Pen/></Button>
+                    <Button className="text-right" variant="outline"><Pen /></Button>
                 </div>
+                <div className='my-3'>
+                    <div className='flex items-center gap-5'>
+                        <Mail />
+                        <span>patel@gmail.com</span>
+                    </div>
+                    <div className='flex items-center gap-5 my-3'>
+                        <Contact />
+                        <span>7874787478</span>
+                    </div>
+                </div>
+                <div className='my-5'>
+                    <h1>Skills</h1>
+                    <div className='flex items-center gap-1'>
+                        {
+                            skills.length !== 0 ? skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>NA</span>
+                        }
+                    </div>
+                </div>
+                <div className='grid w-full max-w-sm items-center gap-1.5'>
+                        <Label className="text-md font-bold">Resume</Label>
+                        {
+                            isResume ? <a target='blank' href='https://youtube.com/@patelmernstack' className='text-blue-500 w-full hover:underline cursor-pointer'>Youtube</a> : <span>NA</span>
+                        }
+                </div>
+            </div>
+            <div className='max-w-4xl mx-auto bg-white rounded-2xl'>
+                <h1>Applied Jobs</h1>
+                <AppliedJobTable/>
             </div>
         </div>
     )
